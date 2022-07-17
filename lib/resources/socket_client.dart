dbf983a92de45fecbc7df57d3a0707c3c9f0d28f
Pratik1099 <pratikagrawal1099@gmail.com>
2022-07-17 15:24:37 +0530

    url

--- lib/resources/socket_client.dart
+++ lib/resources/socket_client.dart
@@ -5,7 +5,7 @@ class SocketClient {
   static SocketClient? _instance;
 
   SocketClient._internal() {
-    socket = IO.io('http://192.168.0.8:3000', <String, dynamic>{
+    socket = IO.io('https://tictaetoegame.herokuapp.com/', <String, dynamic>{
       'transports': ['websocket'],
       'autoConnect': false,
     });
